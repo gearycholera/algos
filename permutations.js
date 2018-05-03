9708ebfa6b31ec32f819795959b2f23a0aaa2188
girikalra <girikalra@gmail.com>
2018-05-03 16:38:22 -0700

    added get all permutations function

---
+++ permutations.js
@@ -0,0 +1,36 @@
+let tests = require('./tests');
+
+var getAllPermutations = function(item, allowDuplicates) {
+  if (allowDuplicates === undefined) allowDuplicates = false;
+  var type = typeof item;
+  var permutations = [];
+  var nextCombo = [];
+  var chars = [];
+
+  if (type === 'string') chars = item.split('');
+  else if (type === 'number') chars = ('' + item).split('');
+  else if (Array.isArray(item)) chars = item.slice();
+  else throw new Error('input not a valid type, must be a number, string, or an array');
+
+  var permutate = function(chars) {
+    if (chars.length === 0) {
+      var perm;
+      if (type === 'number') perm = Number(nextCombo.join(''));
+      else if (type === 'string') perm = nextCombo.join('');
+      else perm = nextCombo.join('');
+      permutations.push(perm);
+    }
+    for (var i = 0 ; i < chars.length ; i++) {
+      chars.push(chars.shift());
+      nextCombo.push(chars[0]);
+      permutate(chars.slice(1));
+      nextCombo.pop();
+    }
+  }
+
+  permutate(chars);
+  if (!allowDuplicates) permutations = Array.from(new Set(permutations));
+  return (type === 'object') ? permutations.map((arr) => arr.split('')) : permutations;
+}
+
+tests.assertArraysEqual(getAllPermutations('abc', false), ['bac,bca,cba,cab,acb,abc'], 'should get all permutations of a string')
