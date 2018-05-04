1523950aff19bd99f169ef936224035989cb5ffa
girikalra <girikalra@gmail.com>
2018-05-03 17:34:53 -0700

    add prime factors

---
+++ primeFactors.js
@@ -0,0 +1,19 @@
+//Question: How could you find all prime factors of a number?
+
+function primeFactors(num) {
+  var factors = [];
+  for (var i = 2; i <= num; i++) {
+    if (num % i === 0) factors.push(i);
+  }
+
+  return factors.filter((num) => isPrime(num))
+}
+
+function isPrime(num) {
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) return false
+  }
+  return true;
+}
+
+console.log(primeFactors(69))
