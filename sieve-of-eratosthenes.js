43e246badd345b4a053ab66fe864f69d01aaf52a
girikalra <girikalra@gmail.com>
2018-05-04 22:57:45 -0700

    added sieve

---
+++ sieve-of-eratosthenes.js
@@ -0,0 +1,19 @@
+var sieve = function(num) {
+  var primes = [];
+
+  var verifyPrime = function(num) {
+    for (var i = 2; i < num ; i++) {
+      if (num % i === 0) return false
+    }
+    return true
+  }
+
+  for (var i = 2; i <= num; i++) {
+    console.log(i, verifyPrime(i))
+    if (verifyPrime(i)) primes.push(i);
+  }
+
+  return primes;
+}
+
+console.log(sieve(10))
